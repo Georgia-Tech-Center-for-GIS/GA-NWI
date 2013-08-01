b8083b4bf0261dca99af532a6c7ed86995613a89
gtg221h <gtg221h@6420ceac-9ccc-ee4b-8d69-bef0c2835792>
2013-08-01 18:00:08 +0000

    essage

--- ArcMapAddin2/ArcGISAddin1.cs
+++ ArcMapAddin2/ArcGISAddin1.cs
@@ -11,6 +11,10 @@ using ESRI.ArcGIS.esriSystem;
 using ESRI.ArcGIS.Geometry;
 
 using System.Collections;
+using System.ComponentModel;
+using ESRI.ArcGIS.Geoprocessor;
+using ESRI.ArcGIS.Geoprocessing;
+using ESRI.ArcGIS.DataSourcesGDB;
 
 namespace GAWetlands
 {
@@ -72,8 +76,28 @@ namespace GAWetlands
 
             return 0;
         }
+        #endregion
+
+        private class DoSymbolizeLayerClass
+        {
+            public System.Collections.Generic.HashSet<string> hsToRemove = new System.Collections.Generic.HashSet<string>();
+            public string symbType = "";
+            public bool IsGPBusy = false;
+        }
+
+        private static DoSymbolizeLayerClass dslc = new DoSymbolizeLayerClass();
+        private static IUniqueValueRenderer urvl = null;
+        private static IFeatureLayer srcLayer = null;
+
+        private static Geoprocessor gp = new Geoprocessor();
 
         public static void doSymbolizeLayer(IActiveView activeView, int i, string symbType) {
+            if (dslc.IsGPBusy)
+            {
+                System.Windows.Forms.MessageBox.Show("Symbolization in progress. Please wait until complete before restarting.");
+                return;
+            }
+
             ESRI.ArcGIS.Carto.IMap map = activeView.FocusMap;
             ESRI.ArcGIS.Carto.ILayerFile layerFile = new ESRI.ArcGIS.Carto.LayerFileClass();
             
@@ -85,70 +109,273 @@ namespace GAWetlands
                 return;
             }
 
+            dslc = new DoSymbolizeLayerClass();
+
             try
             {
-                IFeatureLayer2 ifl2 = (IFeatureLayer2)ArcMap.Document.SelectedLayer;//(IFeatureLayer2)map.get_Layer(i);
-                IGeoFeatureLayer igfl = (IGeoFeatureLayer)ifl2;
-                ITable tbl = (ITable)((IFeatureLayer)igfl).FeatureClass;
+                IFeatureLayer ifl = (IFeatureLayer)ArcMap.Document.SelectedLayer;
+                srcLayer = ifl;
 
-                IQueryFilter iqf = new QueryFilterClass();
-                iqf.WhereClause = "1=1";
-                ICursor csr = (ICursor) ifl2.Search(iqf, true);
+                IFeatureWorkspace iw = (IFeatureWorkspace)((IDataset)ifl.FeatureClass).Workspace;
+
+                ESRI.ArcGIS.AnalysisTools.Statistics stats = new ESRI.ArcGIS.AnalysisTools.Statistics();
+                stats.in_table = "NWI_POLY";
+                stats.out_table = "In_memory\\SymbolStats" + "_" + symbType;
+                stats.case_field = symbType;
+
+                stats.statistics_fields = "\"" + symbType + "\" " + "COUNT";
+
+                gp.AddOutputsToMap = true;
+                gp.OverwriteOutput = true;
+                gp.ToolExecuted += new EventHandler<ToolExecutedEventArgs>(gp_ToolExecuted);
+                gp.ProgressChanged += new EventHandler<ESRI.ArcGIS.Geoprocessor.ProgressChangedEventArgs>(gp_ProgressChanged);
+
+                dslc.symbType = symbType;
+                dslc.hsToRemove = new HashSet<string>();
 
                 string geomTypeName = "Polygon";
 
-                if (ifl2.ShapeType == esriGeometryType.esriGeometryPolyline)
+                if (ifl.FeatureClass.ShapeType == esriGeometryType.esriGeometryPolyline)
                     geomTypeName = "Polyline";
 
-                //layerFile.Open("\\\\tornado\\Research3\\Tony\\Wetlands\\wetlands10.1\\10.0\\" + symbType + "_" + geomTypeName + ".lyr");
                 var asmPath = GetAssemblyPath();
 
                 layerFile.Open(asmPath + "/Symbology/" + symbType + "_" + geomTypeName + ".lyr");
+                IGeoFeatureLayer igfl = (IGeoFeatureLayer)layerFile.Layer;
 
-                IDataStatistics ids = new DataStatisticsClass();
-                ids.Field = symbType;
-                ids.Cursor = csr;
+                urvl = (IUniqueValueRenderer)igfl.Renderer;
 
-                System.Collections.Generic.HashSet<string> hs = new System.Collections.Generic.HashSet<string>();
-
-                IEnumerator iem = ids.UniqueValues;
-
-                while (iem.MoveNext())
+                for (int index = 0; index < urvl.ValueCount; index++)
                 {
-                    //object val = rw.get_Value(rw.Fields.FindField(symbType));
-                    object val = iem.Current;
-
-                    if (val != null)
-                        hs.Add(val.ToString());
+                    dslc.hsToRemove.Add(urvl.Value[index]);
                 }
 
-                //map.AddLayer(layerFile.Layer);
-
-                IGeoFeatureLayer igfl_lyr = (IGeoFeatureLayer)layerFile.Layer;
-                igfl.Renderer = igfl_lyr.Renderer;
-
-                IUniqueValueRenderer urvl = (IUniqueValueRenderer)igfl.Renderer;
-                for (int j = 0; j < urvl.ValueCount; j++)
+                ITable tbl = null;
+                try
                 {
-                    string a = urvl.get_Value(j);
-                    if (a == null) continue;
-
-                    //System.Windows.Forms.MessageBox.Show(a);
-
-                    if (!hs.Contains(a))
-                        urvl.RemoveValue(a);
+                    IGPUtilities igpu = new GPUtilitiesClass();
+                    //IArray tbl = (IArray)igpu.GetGxObjects((string) igpr.ReturnValue);
+                    tbl = igpu.FindMapTable("SymbolStats" + "_" + dslc.symbType);
+                }
+                catch(Exception eee) {
                 }
 
-                ArcMap.Document.CurrentContentsView.Refresh(null);
+                if (tbl == null)
+                    gp.ExecuteAsync((IGPProcess)stats);
+                else
+                {
+                    ProcessResult();
+                }
+                /*ISQLSyntax sql = (ISQLSyntax)iw;
+
+                string prefix = sql.GetSpecialCharacter(esriSQLSpecialCharacters.esriSQL_DelimitedIdentifierPrefix);
+                string suffix = sql.GetSpecialCharacter(esriSQLSpecialCharacters.esriSQL_DelimitedIdentifierSuffix);
+                //ITable tbl = (ITable)((IFeatureLayer)igfl).FeatureClass;
+
+                IQueryFilter iqf = new QueryFilterClass();
+                iqf.WhereClause = "1=1";
+                ICursor csr = (ICursor) ifl.Search(iqf, true);
+
+                dslc.symbType = symbType;
+                dslc.RunWorkerCompleted +=new RunWorkerCompletedEventHandler(dslc_RunWorkerCompleted);
+                dslc.RunWorkerAsync(csr);*/
+#if false
+
+                dslc.hsToRemove.Clear();
+
+                for (int index = 0; index < dslc.urvl.ValueCount; index++)
+                {
+                    dslc.hsToRemove.Add(dslc.urvl.Value[index]);
+                }
+
+                dslc.csr = csr;
+                dslc.symbType = symbType;
+
+                IFeature feature = null;
+                int pos = csr.Fields.FindField(symbType);
+
+                if (pos == -1) //
+                    return;
+
+                while ((feature = csr.NextFeature()) != null)
+                {
+                    dslc.hsToRemove.Remove((string)feature.get_Value(pos));
+
+                    stepProgressor.Step();
+
+                    if (stepProgressor.Position == stepProgressor.MaxRange)
+                    {
+                        stepProgressor.Position = stepProgressor.MinRange;
+                    }
+
+                    if (dslc.hsToRemove.Count == 0)
+                    {
+                        break;
+                    }
+                }
+
+                IUniqueValueRenderer urvl = dslc.urvl;
+
+                foreach (string h in dslc.hsToRemove)
+                {
+                    urvl.RemoveValue(h);
+                }
+
+
+                IGeoFeatureLayer igfl_lyr = (IGeoFeatureLayer)ifl;
+                igfl_lyr.Renderer = (IFeatureRenderer)urvl;
+
+                progressDialog2.HideDialog();
+
+                ArcMap.Document.ActiveView.ContentsChanged();
+                ArcMap.Document.ActiveView.PartialRefresh(esriViewDrawPhase.esriViewGeography, null, null);
+#endif
+#if false
+                IQueryDef2 iqd = (IQueryDef2) ws.CreateQueryDef();
+                iqd.PrefixClause = "DISTINCT";
+                iqd.WhereClause = "1=1";
+                iqd.SubFields = ifl.FeatureClass.OIDFieldName + "," + ifl.FeatureClass.ShapeFieldName + "," + symbType;
+                iqd.Tables = ifl.Name;
+
+                {
+                    IDataStatistics ids = new DataStatisticsClass();
+                    ids.Field = symbType;
+                    ids.Cursor = csr;
+
+                    IEnumerator iem = null;
+
+                    try
+                    {
+                        iem = ids.UniqueValues;
+                    }
+                    catch
+                    {
+                    }
+
+                    if (ids.UniqueValueCount > 0)
+                    {
+                        while (iem.MoveNext())
+                        {
+                            object val = iem.Current;
+
+                            if (val != null && ((string)val).Trim() != "")
+                                hs.Add(val.ToString());
+                        }
+                    }
+
+                    IRow rw = null;
+
+                    int index = csr.Fields.FindField(symbType);
+                    while ((rw = csr.NextRow()) != null)
+                    {
+                        rw.get_Value(index);
+                    }
+
+                    if (hs.Count == 0)
+                    {
+                        System.Windows.Forms.MessageBox.Show("No unique values other than null or blank found. No changes will be made to the symbology");
+                        return;
+                    }
+
+                    for (int j = 0; j < urvl.ValueCount; j++)
+                    {
+                        string a = urvl.get_Value(j);
+                        if (a == null) continue;
+
+                        if (!hs.Contains(a))
+                            urvl.RemoveValue(a);
+                    }
+                }
+#endif
+
+                //ArcMap.Document.CurrentContentsView.Refresh(null);
+                //ArcMap.Document.ActiveView.Refresh();
             }
             catch (System.Exception err)
             {
-
+                object severity = null;
+                string s = gp.GetMessages(ref severity);
             }
             finally
             {
                 if(layerFile != null) layerFile.Close();
             }
+        }
+
+        static void gp_ProgressChanged(object sender, ESRI.ArcGIS.Geoprocessor.ProgressChangedEventArgs e)
+        {
+            IStepProgressor isp = ArcMap.Application.StatusBar.ProgressBar;
+            ArcMap.Application.StatusBar.ProgressBar.Position = Convert.ToInt32((e.ProgressPercentage * (isp.MaxRange - isp.MinRange) + isp.MinRange));
+        }
+
+        static void gp_ToolExecuted(object sender, ToolExecutedEventArgs e)
+        {
+            try {
+                IGeoProcessorResult igpr = e.GPResult;
+
+                if (igpr.Status == esriJobStatus.esriJobSucceeded)
+                {
+                    ProcessResult();
+                }
+
+                if (igpr.Status == esriJobStatus.esriJobFailed || igpr.Status == esriJobStatus.esriJobSucceeded ||
+                    igpr.Status == esriJobStatus.esriJobTimedOut)
+                {
+                    dslc.IsGPBusy = false;
+                }
+            }
+            catch(Exception err) {
+            }
+
+            finally {
+            }
+        }
+
+        private static void ProcessResult()
+        {
+            IGPUtilities igpu = new GPUtilitiesClass();
+            //IArray tbl = (IArray)igpu.GetGxObjects((string) igpr.ReturnValue);
+            ITable tbl = igpu.FindMapTable("SymbolStats" + "_" + dslc.symbType);
+
+            int symbPos = tbl.FindField(dslc.symbType);
+            //int countPos = tbl.FindField("Count_" + dslc.symbType);
+
+            IRow rw = null;
+            IQueryFilter iqf = new QueryFilterClass();
+            iqf.WhereClause = "1=1";
+
+            ICursor csr = tbl.Search(iqf, true);
+
+            int oldCount = dslc.hsToRemove.Count;
+
+            while ((rw = csr.NextRow()) != null)
+            {
+                dslc.hsToRemove.Remove((string)rw.get_Value(symbPos));
+            }
+
+            if (dslc.hsToRemove.Count == oldCount)
+            {
+                System.Windows.Forms.MessageBox.Show("No unique values other than null or blank found. No changes will be made to the symbology");
+                return;
+            }
+
+            for (int j = 0; j < urvl.ValueCount; j++)
+            {
+                string a = urvl.get_Value(j);
+                if (a == null) continue;
+
+                if (dslc.hsToRemove.Contains(a))
+                    urvl.RemoveValue(a);
+            }
+
+            IGeoFeatureLayer igd_dest = (IGeoFeatureLayer)srcLayer;
+            igd_dest.Renderer = (IFeatureRenderer)urvl;
+
+            ArcMap.Document.CurrentContentsView.Deactivate();
+            ArcMap.Document.ContentsView[0].Activate(ArcMap.Application.hWnd, ArcMap.Document);
+
+            ArcMap.Document.ActiveView.ContentsChanged();
+            ArcMap.Document.ActiveView.PartialRefresh(esriViewDrawPhase.esriViewGeography, null, null);
         }
 
         private static string GetAssemblyPath()
@@ -159,115 +386,10 @@ namespace GAWetlands
             asmPath = System.IO.Path.GetDirectoryName(asmPath);
             return asmPath;
         }
-        #endregion
-
-        static void DefineUniqueValueRenderer(IGeoFeatureLayer pGeoFeatureLayer, string
-            fieldName)
-        {
-            IRandomColorRamp pRandomColorRamp = new RandomColorRampClass();
-            //Create the color ramp for the symbols in the renderer.
-            pRandomColorRamp.MinSaturation = 20;
-            pRandomColorRamp.MaxSaturation = 40;
-            pRandomColorRamp.MinValue = 85;
-            pRandomColorRamp.MaxValue = 100;
-            pRandomColorRamp.StartHue = 76;
-            pRandomColorRamp.EndHue = 188;
-            pRandomColorRamp.UseSeed = true;
-            pRandomColorRamp.Seed = 43;
-
-            //Create the renderer.
-            IUniqueValueRenderer pUniqueValueRenderer = new UniqueValueRendererClass();
-
-            ISimpleFillSymbol pSimpleFillSymbol = new SimpleFillSymbolClass();
-            pSimpleFillSymbol.Style = esriSimpleFillStyle.esriSFSSolid;
-            pSimpleFillSymbol.Outline.Width = 0.4;
-
-            //These properties should be set prior to adding values.
-            pUniqueValueRenderer.FieldCount = 1;
-            pUniqueValueRenderer.set_Field(0, fieldName);
-            pUniqueValueRenderer.DefaultSymbol = pSimpleFillSymbol as ISymbol;
-            pUniqueValueRenderer.UseDefaultSymbol = true;
-
-            IDisplayTable pDisplayTable = pGeoFeatureLayer as IDisplayTable;
-            IFeatureCursor pFeatureCursor = pDisplayTable.SearchDisplayTable(null, false) as
-                IFeatureCursor;
-            IFeature pFeature = pFeatureCursor.NextFeature();
-
-
-            bool ValFound;
-            int fieldIndex;
-
-            IFields pFields = pFeatureCursor.Fields;
-            fieldIndex = pFields.FindField(fieldName);
-            while (pFeature != null)
-            {
-                ISimpleFillSymbol pClassSymbol = new SimpleFillSymbolClass();
-                pClassSymbol.Style = esriSimpleFillStyle.esriSFSSolid;
-                pClassSymbol.Outline.Width = 0.4;
-
-                string classValue;
-                classValue = pFeature.get_Value(fieldIndex) as string;
-
-                //Test to see if this value was added to the renderer. If not, add it.
-                ValFound = false;
-                for (int i = 0; i <= pUniqueValueRenderer.ValueCount - 1; i++)
-                {
-                    if (pUniqueValueRenderer.get_Value(i) == classValue)
-                    {
-                        ValFound = true;
-                        break; //Exit the loop if the value was found.
-                    }
-                }
-                //If the value was not found, it's new and will be added.
-                if (ValFound == false)
-                {
-                    pUniqueValueRenderer.AddValue(classValue, fieldName, pClassSymbol as
-                        ISymbol);
-                    pUniqueValueRenderer.set_Label(classValue, classValue);
-                    pUniqueValueRenderer.set_Symbol(classValue, pClassSymbol as ISymbol);
-                }
-                pFeature = pFeatureCursor.NextFeature();
-            }
-            //Since the number of unique values is known, the color ramp can be sized and the colors assigned.
-            pRandomColorRamp.Size = pUniqueValueRenderer.ValueCount;
-            bool bOK;
-            pRandomColorRamp.CreateRamp(out bOK);
-
-            IEnumColors pEnumColors = pRandomColorRamp.Colors;
-            pEnumColors.Reset();
-            for (int j = 0; j <= pUniqueValueRenderer.ValueCount - 1; j++)
-            {
-                string xv;
-                xv = pUniqueValueRenderer.get_Value(j);
-                if (xv != "")
-                {
-                    ISimpleFillSymbol pSimpleFillColor = pUniqueValueRenderer.get_Symbol(xv)
-                        as ISimpleFillSymbol;
-                    pSimpleFillColor.Color = pEnumColors.Next();
-                    pUniqueValueRenderer.set_Symbol(xv, pSimpleFillColor as ISymbol);
-
-                }
-            }
-
-            //'** If you didn't use a predefined color ramp in a style, use "Custom" here. 
-            //'** Otherwise, use the name of the color ramp you selected.
-            pUniqueValueRenderer.ColorScheme = "Custom";
-            ITable pTable = pDisplayTable as ITable;
-            bool isString = pTable.Fields.get_Field(fieldIndex).Type ==
-                esriFieldType.esriFieldTypeString;
-            pUniqueValueRenderer.set_FieldType(0, isString);
-            pGeoFeatureLayer.Renderer = pUniqueValueRenderer as IFeatureRenderer;
-
-            //This makes the layer properties symbology tab show the correct interface.
-            IUID pUID = new UIDClass();
-            pUID.Value = "{683C994E-A17B-11D1-8816-080009EC732A}";
-            pGeoFeatureLayer.RendererPropertyPageClassID = pUID as UIDClass;
-        }
 
         protected void doSymbolize(string type)
         {
              IActiveView iav = ArcMap.Document.ActiveView;
-             //GetIndexNumberFromLayerName(iav, "NWI_POLY");
              doSymbolizeLayer(iav, 0, type);
         }
 
